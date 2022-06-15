21b9d2a914f4d4c7520238e9f1749ab0e833898c
Renato <m.renato@freemail.hu>
2022-06-15 20:03:13 +0200

    Initial commit

---
+++ springs-sky-travel/src/main/java/springsskytravel/SpringsSkyTravelApplication.java
@@ -0,0 +1,13 @@
+package springsskytravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsSkyTravelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringsSkyTravelApplication.class, args);
+    }
+
+}
