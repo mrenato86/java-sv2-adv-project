3b67513f6f3045c3c59d72daa62c2f9bb0077c9a
Renato <m.renato@freemail.hu>
2022-06-20 01:41:52 +0200

    Deprecated function call changed in queries

--- springs-sky-travel/src/main/java/springsskytravel/repositories/ReservationRepository.java
+++ springs-sky-travel/src/main/java/springsskytravel/repositories/ReservationRepository.java
@@ -10,9 +10,9 @@ import java.util.Optional;
 
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
-    @Query("select r from Reservation r where (r.journey.departureDate >= CURRENT_DATE) and (:group is null or r.participants.size <= :group)")
+    @Query("select r from Reservation r where (r.journey.departureDate >= CURRENT_DATE) and (:group is null or size(r.participants) <= :group)")
     List<Reservation> getActiveReservationsWithParticipantsLessThan(Optional<Integer> group);
 
-    @Query("select r from Reservation r where :group is null or r.participants.size <= :group")
+    @Query("select r from Reservation r where :group is null or size(r.participants) <= :group")
     List<Reservation> getReservationsWithParticipantsLessThan(Optional<Integer> group);
 }
